13cfcf25b90721dfd367312b6bfb6ce06b13c790
Andy Burke <andy.burke@mailbox.earth>
2021-06-15 00:26:09 -0700

    feature: first network replay of events

--- client/RPMBridge.cs
+++ client/RPMBridge.cs
@@ -2,14 +2,16 @@
 using KSP;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace space_with_friends {
 
 	[KSPAddon( KSPAddon.Startup.Flight, once: true )]
 	public class RPMBridge : MonoBehaviour {
-	    private static readonly fsSerializer _serializer = new fsSerializer();
-        private static readonly ConcurrentDictionary<Guid, bool> replaying = new ConcurrentDictionary<Guid, bool>();
+		private static readonly fsSerializer _serializer = new fsSerializer();
+		private static readonly ConcurrentDictionary<Guid, bool> replaying = new ConcurrentDictionary<Guid, bool>();
+		private static Queue<Action> run_in_main = new Queue<Action>();
 
 		public void Start() {
 			DontDestroyOnLoad( this );
@@ -23,6 +25,18 @@ namespace space_with_friends {
 			utils.Log( "unbinding RPM events", "RPMBridge" );
 			Core.client.on_message -= on_network_message;
 			JSI.Core.Events.onEvent -= on_event;
+		}
+
+		public void Update() {
+			while( run_in_main.Count > 0 )
+			{
+				Action action = null;
+				lock( run_in_main )
+				{
+					action = run_in_main.Dequeue();
+				}
+				action?.Invoke();
+			}
 		}
 
 		public static void on_event( JSI.Core.Event _event ) {
@@ -51,7 +65,7 @@ namespace space_with_friends {
 		void on_network_message( string from, object msg ) {
 			utils.Log( $"net from: { from } type: { msg.GetType() }", "RPMBridge" );
 			if ( msg is msg.rpm_event rpm_event ) {
-				utils.Log( "  is rpm_event (", "RPMBridge" );
+
 				try {
 					fsData data = fsJsonParser.Parse( rpm_event.event_json );
 
@@ -66,25 +80,27 @@ namespace space_with_friends {
 						return;
 					}
 
-					replaying.TryAdd( _event.id, true );
+					run_in_main.Enqueue( () => {
+						replaying.TryAdd( _event.id, true );
 
-					switch( _event.type ) {
-						case "click":
-						case "release":
-							try {
-								utils.Log( $"replay: {rpm_event.player_id}: { _event.type } { _event.vessel_id.ToString() } { ( (JSI.Core.EventData)_event.data ).propID } { ( (JSI.Core.EventData)_event.data ).buttonName }", "RPMBridge" );
-								JSI.SmarterButton.Replay( _event );
-							}
-							catch( Exception ex ) {
-								UnityEngine.Debug.LogError( ex.StackTrace );
-							}
-							break;
-						default:
-							utils.Log( "  unknown type: " + _event.type, "RPMBridge" );
-							break;
-					}
+						switch( _event.type ) {
+							case "click":
+							case "release":
+								try {
+									utils.Log( $"replay: {rpm_event.player_id}: { _event.type } { _event.vessel_id.ToString() } { ( (JSI.Core.EventData)_event.data ).propID } { ( (JSI.Core.EventData)_event.data ).buttonName }", "RPMBridge" );
+									JSI.SmarterButton.Replay( _event );
+								}
+								catch( Exception ex ) {
+									UnityEngine.Debug.LogError( ex.StackTrace );
+								}
+								break;
+							default:
+								utils.Log( "  unknown type: " + _event.type, "RPMBridge" );
+								break;
+						}
 
-					replaying.TryRemove( _event.id, out var removed );
+						replaying.TryRemove( _event.id, out var removed );
+					} );
 				}
 				catch (System.Exception ex) {
 					UnityEngine.Debug.LogError( ex.StackTrace );
