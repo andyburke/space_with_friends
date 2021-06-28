ec83900cc79dc60d77a40a4ddccb9e567b63df6b
Andy Burke <andy.burke@mailbox.earth>
2021-06-28 02:05:46 -0700

    refactor: vessel creation/destruction isn't what we really want, need to track it going to the launchpad, which rollout seems to do, problem is now the kerbal crew

--- client/VesselBridge.cs
+++ client/VesselBridge.cs
@@ -34,11 +34,14 @@ namespace space_with_friends {
 
 			DontDestroyOnLoad( this );
 
+			GameEvents.onFlightReady.Add( this.on_flight_ready );
 			GameEvents.onNewVesselCreated.Add( this.on_new_vessel_created );
+			GameEvents.onVesselChange.Add( this.on_vessel_change );
 			GameEvents.onVesselCreate.Add( this.on_vessel_create );
 			GameEvents.onVesselDestroy.Add( this.on_vessel_destroy );
 			GameEvents.onVesselLoaded.Add( this.on_vessel_loaded );
 			GameEvents.onVesselRecovered.Add( this.on_vessel_recovered );
+			GameEvents.OnVesselRollout.Add( this.on_vessel_rollout );
 			GameEvents.onVesselTerminated.Add( this.on_vessel_terminated );
 			GameEvents.onVesselWasModified.Add( this.on_vessel_was_modified );
 
@@ -51,11 +54,14 @@ namespace space_with_friends {
 
 			GameEvents.onVesselWasModified.Remove( this.on_vessel_was_modified );
 			GameEvents.onVesselTerminated.Remove( this.on_vessel_terminated );
+			GameEvents.OnVesselRollout.Remove( this.on_vessel_rollout );
 			GameEvents.onVesselRecovered.Remove( this.on_vessel_recovered );
 			GameEvents.onVesselLoaded.Remove( this.on_vessel_loaded );
 			GameEvents.onVesselDestroy.Remove( this.on_vessel_destroy );
 			GameEvents.onVesselCreate.Remove( this.on_vessel_create );
+			GameEvents.onVesselChange.Remove( this.on_vessel_change );
 			GameEvents.onNewVesselCreated.Remove( this.on_new_vessel_created );
+			GameEvents.onFlightReady.Remove( this.on_flight_ready );
 
 			Core.client.on_message -= on_message;
 			initialized = false;
@@ -74,15 +80,11 @@ namespace space_with_friends {
 		}
 
 		public static void on_message( space_with_friends.msg msg ) {
-			if ( !HighLogic.LoadedSceneIsFlight ) {
-				return;
-			}
-
 			switch( msg.type ) {
-				case "vessel_create":
+				case "vessel_rollout": {
 					utils.Log( $"vessel_create: ${ msg.vessel_id }" );
 					ProtoVessel created_protovessel = deserialize_protovessel( msg.message );
-                    Vessel existing_vessel = FlightGlobals.FindVessel( created_protovessel.vesselID );
+					Vessel existing_vessel = FlightGlobals.FindVessel( created_protovessel.vesselID );
 					if ( existing_vessel != null ) {
 						utils.Log( "  exists, skipping" );
 						return;
@@ -146,11 +148,43 @@ namespace space_with_friends {
 							}
 						}
 					}
+				}
 					break;
-				case "vessel_destroy":
+				case "vessel_terminated": {
+					utils.Log( $"removing vessel: ${ msg.vessel_id }" );
+					Vessel existing_vessel = FlightGlobals.Vessels.Find( v => v.id == msg.vessel_id );
+					if ( existing_vessel == null ) {
+						utils.Log( "  doesn't exist, skipping" );
+						return;
+					}
+
+					try
+					{
+						if ( FlightGlobals.fetch?.VesselTarget?.GetVessel().id == existing_vessel.id ) {
+							FlightGlobals.fetch.SetVesselTarget( null );
+						}
+
+						FlightGlobals.RemoveVessel( existing_vessel );
+						foreach ( var part in existing_vessel.parts ) {
+							UnityEngine.Object.Destroy( part.gameObject );
+						}
+						UnityEngine.Object.Destroy( existing_vessel.gameObject );
+
+						HighLogic.CurrentGame.flightState.protoVessels.RemoveAll( v => v?.vesselID == existing_vessel.id );
+						KSCVesselMarkers.fetch?.RefreshMarkers();
+					}
+					catch ( Exception ex )
+					{
+						utils.Log( $"error destroying vessel: { ex }" );
+						return;
+					}
+
+					utils.Log( $"  destroyed" );
+				}
+					break;
+				case "vessel_create":
 				case "vessel_loaded":
 				case "vessel_recovered":
-				case "vessel_terminated":
 				case "vessel_modified":
 					break;
 				default:
@@ -164,8 +198,35 @@ namespace space_with_friends {
 		// pilot_node.AddValue( "pilot", space_with_friends.Core.player_id );
 		// vessel.AddNode( "swf_node", pilot_node );
 
+		public void on_flight_ready() {
+			utils.Log( $"flight ready: { FlightGlobals.ActiveVessel?.id.ToString() }" );
+
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = FlightGlobals.ActiveVessel?.id ?? Guid.Empty,
+				type = "vessel_flight_ready",
+				message = ""
+			} );
+		}
+
 		public void on_new_vessel_created( Vessel vessel ) {
 			utils.Log( $"new vessel created: { vessel.id.ToString() }" );
+		}
+
+		public void on_vessel_change( Vessel vessel ) {
+			utils.Log( $"vessel change: { vessel.id.ToString() }" );
+
+			ProtoVessel protovessel = vessel.protoVessel;
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = vessel.id,
+				type = "vessel_change",
+				message = ""
+			} );
 		}
 
 		public void on_vessel_create( Vessel vessel ) {
@@ -178,28 +239,98 @@ namespace space_with_friends {
 				world_time = Planetarium.GetUniversalTime(),
 				vessel_id = vessel.id,
 				type = "vessel_create",
-				message = serialize_protovessel( protovessel )
+				message = ""
 			} );
 		}
 
 		public void on_vessel_destroy( Vessel vessel ) {
 			utils.Log( $"vessel destroy: { vessel.id.ToString() }" );
+			ProtoVessel protovessel = vessel.protoVessel;
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = vessel.id,
+				type = "vessel_destroy",
+				message = ""
+			} );
 		}
 
 		public void on_vessel_loaded( Vessel vessel ) {
 			utils.Log( $"vessel loaded: { vessel.id.ToString() }" );
+			ProtoVessel protovessel = vessel.protoVessel;
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = vessel.id,
+				type = "vessel_loaded",
+				message = ""
+			} );
 		}
 
-		public void on_vessel_recovered( ProtoVessel vessel, bool arg ) {
-			utils.Log( $"vessel recovered: { vessel.vesselID.ToString() } { arg }" );
+		public void on_vessel_recovered( ProtoVessel protovessel, bool arg ) {
+			utils.Log( $"vessel recovered: { protovessel.vesselID.ToString() } { arg }" );
+
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = protovessel.vesselID,
+				type = "vessel_recovered",
+				message = ""
+			} );
 		}
 
-		public void on_vessel_terminated( ProtoVessel vessel ) {
-			utils.Log( $"vessel terminated: { vessel.vesselID.ToString() }" );
+		public void on_vessel_rollout( ShipConstruct ship_construct ) {
+			Vessel vessel = ship_construct.Parts?[ 0 ]?.vessel;
+
+			utils.Log( $"vessel rollout: { ship_construct.shipName } { vessel?.id }" );
+			utils.Log( $"  current active vessel: { FlightGlobals.ActiveVessel?.id }" );
+
+			// TODO: set a timeout of a few ms, then get the active vessel and send it (or match something from the ship_construct to verify)
+			// TODO: move vessel serialization here
+
+			ProtoVessel protovessel = vessel?.protoVessel;
+			if ( protovessel == null ) {
+				utils.Log( "  no protovessel" );
+				return;
+			}
+
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = protovessel.vesselID,
+				type = "vessel_rollout",
+				message = serialize_protovessel( protovessel )
+			} );
+		}
+
+		public void on_vessel_terminated( ProtoVessel protovessel ) {
+			utils.Log( $"vessel terminated: { protovessel.vesselID.ToString() }" );
+
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = protovessel.vesselID,
+				type = "vessel_terminated",
+				message = ""
+			} );
 		}
 
 		public void on_vessel_was_modified( Vessel vessel ) {
 			utils.Log( $"vessel was modified: { vessel.id.ToString() }" );
+			ProtoVessel protovessel = vessel.protoVessel;
+			Core.client?.send( new msg {
+				world_id = space_with_friends.Core.world_id,
+				source = space_with_friends.Core.player_id,
+				world_time = Planetarium.GetUniversalTime(),
+				vessel_id = vessel.id,
+				type = "vessel_was_modified",
+				message = ""
+			} );
 		}
 
 		public static string serialize_protovessel( ProtoVessel protovessel ) {
